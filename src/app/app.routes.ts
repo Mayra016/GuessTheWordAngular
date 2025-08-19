e9d46bdeb907bf1eda6b73036d837ea7412d1384
Mayra016 <mayrazucaro@gmail.com>
2025-08-19 11:00:41 -0300

    Feature: create project basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlayComponent } from './play/play.component';
+import { LanguageComponent } from './language/language.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { InfoComponent } from './info/info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'menu', component: HomeComponent},
+    {path: 'play', component: PlayComponent},
+    {path: 'configuration', component: ConfigurationComponent},
+    {path: 'information', component: InformationComponent},
+    {path: '**', redirectTo: 'menu'}
+];
