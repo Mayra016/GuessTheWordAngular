1ad8dcecee91d4c15db5068253a8dc4062f062c4
Mayra016 <mayrazucaro@gmail.com>
2025-08-20 09:58:15 -0300

    Fix: route names

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PlayComponent } from './play/play.component';
-import { LanguageComponent } from './language/language.component';
 import { ConfigurationComponent } from './configuration/configuration.component';
-import { InfoComponent } from './info/info.component';
+import { InformationComponent } from './information/information.component';
 
 export const routes: Routes = [
     {path: 'menu', component: HomeComponent},
